154f91296ad27133c7b012db30e81e32dda0c438
jagadhis <jwaran78@gmail.com>
2022-05-29 12:16:07 +0530

    changed Table

--- src/components/Table/StyledTable.js
+++ src/components/Table/StyledTable.js
@@ -2,27 +2,42 @@ import styled from "styled-components";
 
 export const StyledTable = styled.table`
   border: 1px solid #fff;
+  border-radius: 3px;
 `;
 
 export const THead = styled.thead`
   border: 1px solid #fff;
   color: white;
+
+  justify-content: center;
 `;
 
 export const TFoot = styled.tfoot`
-  // custom css goes here
+  border: 1px solid #fff;
+  color: white;
+
+  justify-content: center;
 `;
 
 export const TBody = styled.tbody`
-  // custom css goes here
+  border: 1px solid #fff;
+  color: white;
+
+  justify-content: center;
 `;
 
 export const TR = styled.tr`
-  // custom css goes here
+  border: 1px solid #fff;
+  color: white;
+
+  justify-content: center;
 `;
 
 export const TH = styled.th`
-  // custom css goes here
+  border: 1px solid #fff;
+  color: white;
+
+  justify-content: center;
 `;
 
 export const TD = styled.td`
