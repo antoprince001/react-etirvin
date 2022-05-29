b9ba72250317ecaa079e6ac5b0f2dfee94d97cc9
jagadhis <jwaran78@gmail.com>
2022-05-29 09:43:20 +0530

    working on Lists

--- src/components/ListGroup/OrderedList.js
+++ src/components/ListGroup/OrderedList.js
@@ -2,6 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 const OrderedlistStyle = styled.ul`
+  @counter-style dotless-item {
+    system: numeric;
+    symbols: "0" "1" "2" "3" "4" "5" "6" "7" "8" "9";
+    suffix: " ";
+  }
+  content: counter(list-item) "\A0";
+  float: left;
+  text-align: right;
+  width: 1.5em;
   .dropbtn {
     font-size: 16px;
   }
--- src/components/ListGroup/UnorderedList.js
+++ src/components/ListGroup/UnorderedList.js
@@ -2,6 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 const UnOrderedlistStyle = styled.ol`
+  ${
+    "" /* *list-style-type: decimal;
+  margin-left: 0;
+  counter-reset: customlistcounter;
+  counter-increment: customlistcounter;
+  content: counter(customlistcounter) " ";
+  font-weight: bold;
+  float: left;
+  width: 3em; */
+  }
+
   .dropbtn {
     font-size: 16px;
   }
@@ -10,6 +21,7 @@ const UnOrderedlist = ({ title, content }) => {
   return (
     <UnOrderedlistStyle>
       <ol className="dropdown">hello</ol>
+      <ol>Hello</ol>
     </UnOrderedlistStyle>
   );
 };
